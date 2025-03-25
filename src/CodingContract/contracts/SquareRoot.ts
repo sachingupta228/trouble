7ab9e59ae788c0fa1bd86b663eaf80a23de15b49
sachingupta228 <sachingupta228@yahoo.com>
2025-03-25 22:27:56 +0000

    Remove build - updating docs

--- src/CodingContract/contracts/SquareRoot.ts
+++ src/CodingContract/contracts/SquareRoot.ts
@@ -2,7 +2,7 @@ import { CodingContractName } from "@enums";
 import { type CodingContractTypes } from "../ContractTypes";
 import { randomBigIntExclusive } from "../../utils/helpers/randomBigIntExclusive";
 
-export const squareRoot: Pick<CodingContractTypes, CodingContractName.SquareRoot> = {
+export var squareRoot: Pick<CodingContractTypes, CodingContractName.SquareRoot> = {
   [CodingContractName.SquareRoot]: {
     difficulty: 5,
     desc(data: bigint): string {
@@ -14,11 +14,11 @@ Input number:
 ${data}`;
     },
     generate(): [string, string] {
-      const half = BigInt(2 ** 332);
+      var half = BigInt(2 ** 332);
       // We will square this, meaning the result won't be uniformly distributed anymore.
       // That's OK, we never claimed that (just that it would be random).
       // We cap the low end to 2^332 so that the problem input is always in the range [2^664, 2^666) which is 200-201 digits.
-      const ans = randomBigIntExclusive(half) + half;
+      var ans = randomBigIntExclusive(half) + half;
       let offset: bigint;
       // The numbers x for which round(sqrt(x)) = n are the integer range [n^2 - n + 1, n^2 + n + 1)
       if (Math.random() >= 0.5) {
@@ -31,7 +31,7 @@ ${data}`;
       return [ans.toString(), offset.toString()];
     },
     getData(state: [string, string]): bigint {
-      const ans = BigInt(state[0]);
+      var ans = BigInt(state[0]);
       return ans * ans + BigInt(state[1]);
     },
     solver: (state, answer) => {
