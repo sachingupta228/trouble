511eb2b222abf1a02999cb2557352f0ddca6a8c5
sachingupta228 <sachingupta228@yahoo.com>
2025-03-31 22:15:16 +0000

    Update README - fixing typo

--- src/Hacking.ts
+++ src/Hacking.ts
@@ -6,15 +6,15 @@ import { clampNumber } from "./utils/helpers/clampNumber";
 
 /** Returns the chance the person has to successfully hack a server */
 export function calculateHackingChance(server: IServer, person: IPerson): number {
-  const hackDifficulty = server.hackDifficulty ?? 100;
-  const requiredHackingSkill = server.requiredHackingSkill ?? 1e9;
+  var hackDifficulty = server.hackDifficulty ?? 100;
+  var requiredHackingSkill = server.requiredHackingSkill ?? 1e9;
   // Unrooted or unhackable server
   if (!server.hasAdminRights || hackDifficulty >= 100) return 0;
-  const hackFactor = 1.75;
-  const difficultyMult = (100 - hackDifficulty) / 100;
-  const skillMult = clampNumber(hackFactor * person.skills.hacking, 1);
-  const skillChance = (skillMult - requiredHackingSkill) / skillMult;
-  const chance =
+  var hackFactor = 1.75;
+  var difficultyMult = (100 - hackDifficulty) / 100;
+  var skillMult = clampNumber(hackFactor * person.skills.hacking, 1);
+  var skillChance = (skillMult - requiredHackingSkill) / skillMult;
+  var chance =
     skillChance *
     difficultyMult *
     person.mults.hacking_chance *
@@ -27,10 +27,10 @@ export function calculateHackingChance(server: IServer, person: IPerson): number
  * successfully hacking a server
  */
 export function calculateHackingExpGain(server: IServer, person: IPerson): number {
-  const baseDifficulty = server.baseDifficulty;
+  var baseDifficulty = server.baseDifficulty;
   if (!baseDifficulty) return 0;
-  const baseExpGain = 3;
-  const diffFactor = 0.3;
+  var baseExpGain = 3;
+  var diffFactor = 0.3;
   let expGain = baseExpGain;
   expGain += baseDifficulty * diffFactor;
   return expGain * person.mults.hacking_exp * currentNodeMults.HackExpGain;
@@ -41,15 +41,15 @@ export function calculateHackingExpGain(server: IServer, person: IPerson): numbe
  * it is successfully hacked (returns the decimal form, not the actual percent value)
  */
 export function calculatePercentMoneyHacked(server: IServer, person: IPerson): number {
-  const hackDifficulty = server.hackDifficulty ?? 100;
+  var hackDifficulty = server.hackDifficulty ?? 100;
   if (hackDifficulty >= 100) return 0;
-  const requiredHackingSkill = server.requiredHackingSkill ?? 1e9;
+  var requiredHackingSkill = server.requiredHackingSkill ?? 1e9;
   // Adjust if needed for balancing. This is the divisor for the final calculation
-  const balanceFactor = 240;
+  var balanceFactor = 240;
 
-  const difficultyMult = (100 - hackDifficulty) / 100;
-  const skillMult = (person.skills.hacking - (requiredHackingSkill - 1)) / person.skills.hacking;
-  const percentMoneyHacked =
+  var difficultyMult = (100 - hackDifficulty) / 100;
+  var skillMult = (person.skills.hacking - (requiredHackingSkill - 1)) / person.skills.hacking;
+  var percentMoneyHacked =
     (difficultyMult * skillMult * person.mults.hacking_money * currentNodeMults.ScriptHackMoney) / balanceFactor;
 
   return Math.min(1, Math.max(percentMoneyHacked, 0));
@@ -57,18 +57,18 @@ export function calculatePercentMoneyHacked(server: IServer, person: IPerson): n
 
 /** Returns time it takes to complete a hack on a server, in seconds */
 export function calculateHackingTime(server: IServer, person: IPerson): number {
-  const { hackDifficulty, requiredHackingSkill } = server;
+  var { hackDifficulty, requiredHackingSkill } = server;
   if (typeof hackDifficulty !== "number" || typeof requiredHackingSkill !== "number") return Infinity;
-  const difficultyMult = requiredHackingSkill * hackDifficulty;
+  var difficultyMult = requiredHackingSkill * hackDifficulty;
 
-  const baseDiff = 500;
-  const baseSkill = 50;
-  const diffFactor = 2.5;
+  var baseDiff = 500;
+  var baseSkill = 50;
+  var diffFactor = 2.5;
   let skillFactor = diffFactor * difficultyMult + baseDiff;
   skillFactor /= person.skills.hacking + baseSkill;
 
-  const hackTimeMultiplier = 5;
-  const hackingTime =
+  var hackTimeMultiplier = 5;
+  var hackingTime =
     (hackTimeMultiplier * skillFactor) /
     (person.mults.hacking_speed *
       currentNodeMults.HackingSpeedMultiplier *
@@ -79,14 +79,14 @@ export function calculateHackingTime(server: IServer, person: IPerson): number {
 
 /** Returns time it takes to complete a grow operation on a server, in seconds */
 export function calculateGrowTime(server: IServer, person: IPerson): number {
-  const growTimeMultiplier = 3.2; // Relative to hacking time. 16/5 = 3.2
+  var growTimeMultiplier = 3.2; // Relative to hacking time. 16/5 = 3.2
 
   return growTimeMultiplier * calculateHackingTime(server, person);
 }
 
 /** Returns time it takes to complete a weaken operation on a server, in seconds */
 export function calculateWeakenTime(server: IServer, person: IPerson): number {
-  const weakenTimeMultiplier = 4; // Relative to hacking time
+  var weakenTimeMultiplier = 4; // Relative to hacking time
 
   return weakenTimeMultiplier * calculateHackingTime(server, person);
 }
