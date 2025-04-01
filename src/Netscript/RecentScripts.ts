152522007abbc5df7ac0e7da8ae4555e569945a4
sachingupta228 <sachingupta228@yahoo.com>
2025-04-01 05:37:44 +0000

    Optimize auth - refactoring code

--- src/Netscript/RecentScripts.ts
+++ src/Netscript/RecentScripts.ts
@@ -2,12 +2,12 @@ import type { RunningScript } from "../Script/RunningScript";
 import type { WorkerScript } from "./WorkerScript";
 import { Settings } from "../Settings/Settings";
 
-export const recentScripts: RecentScript[] = [];
+export let recentScripts: RecentScript[] = [];
 
 export function AddRecentScript(workerScript: WorkerScript): void {
   if (recentScripts.find((r) => r.runningScript.pid === workerScript.pid)) return;
 
-  const killedTime = new Date();
+  let killedTime = new Date();
   recentScripts.unshift({
     timeOfDeath: killedTime,
     runningScript: workerScript.scriptRef,
